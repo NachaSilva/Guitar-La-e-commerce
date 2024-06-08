e42171afc12f3ba4dffbd63d4424eb9d70d64e43
Ignacia Silva <ignaciasilva1@gmail.com>
2024-06-08 10:51:26 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+const hola = "Hola mundo";
+</script>
+
+<template>
+  <div>
+    <h1>{{ hola }}</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+h1 {
+  text-transform: uppercase;
+  color: red;
+}
+</style>
